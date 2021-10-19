4275b4324671e102e15929cebce3a53135cc8dae
rahul <rahul.cn99@gmail.com>
2021-10-19 13:55:53 +0530

    h

---
+++ src/Fibounci.java
@@ -0,0 +1,33 @@
+
+public class Fibounci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int count=5;
+
+while(count>0) {
+	System.out.println("TOP WHILE");
+	int count1=5;
+	while(count1>0)
+	{
+		
+		System.out.println("bOTTOM WHILE");
+		count1--;
+	}
+	count--;
+}
+hi(bye(byee(20)));
+}
+	public static void hi(int a)
+	{
+		//return 20;
+	}
+	public static int bye(int a)
+	{
+		return 20;
+	}
+	public static int byee(int a)
+	{
+		return 20;
+	}
+}
